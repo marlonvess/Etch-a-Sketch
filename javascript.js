e93d3d7c3d599b63cdf266abb9ae5e1dcfac0e90
marlonvess <marlonvess@gmail.com>
2024-02-13 18:27:07 +0000

    First steps,clicable div to change color

---
+++ javascript.js
@@ -0,0 +1,6 @@
+  document.getElementById("container").addEventListener ("click", colorChange);
+  function colorChange(){
+    let color = document.getElementById("container");
+    color.style.backgroundColor = "#D4DBDD";
+  }
+   // 
