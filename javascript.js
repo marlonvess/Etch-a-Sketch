d229ffe80fc146627aac0655b2317ca0f34ba387
marlonvess <marlonvess@gmail.com>
2024-02-19 10:47:14 +0000

    Color change with mouseover

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
-  document.getElementById("container").addEventListener ("click", colorChange);
-  function colorChange(){
-    let color = document.getElementById("container");
-    color.style.backgroundColor = "#D4DBDD";
-  }
-   // 
+
+
+//Multiply all divs to make a 16x16 grid, which is formated on CSS
+for (let i = 0; i <(16*16); i++) {
+    let newDiv=document.createElement('div');
+    newDiv.className='divGrid';
+    document.getElementById("container").appendChild(newDiv);
+}
+
+var hoverDivs = document.querySelectorAll('.divGrid');
+
+    // Add a mouseover event listener to each div with the class "hoverDiv"
+    hoverDivs.forEach(function(div) {
+      div.addEventListener('mouseover', function() {
+            div.style.backgroundColor = "#393737";
+      });
+    });
+
+  
