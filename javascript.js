6325f9f7e6c202df242d94dc3ea36d43f7b9d347
marlonvess <marlonvess@gmail.com>
2024-03-03 18:26:42 +0000

    Slider to change grid size fully working

--- javascript.js
+++ javascript.js
@@ -1,16 +1,44 @@
-//Multiply all divs to make a 16x16 grid, which is formated on CSS
-for (let i = 0; i < 15*15; i++) {
-    let newDiv=document.createElement('div');
-    newDiv.className='divGrid';
-    document.getElementById("container").appendChild(newDiv);
-}
+let sliderInput = document.getElementById('numberSlide');
+let sliderOutput = document.getElementById('output');
+let container = document.getElementById('container');
 
-var hoverDivs = document.querySelectorAll('.divGrid');
+function createGrid(size) {
+  // Clear existing divs in the container
+  container.innerHTML = "";
 
-    // Add a mouseover event listener to each div with the class "hoverDiv"
-    hoverDivs.forEach(function(div) {
-      div.addEventListener('click', function() {
-            div.style.backgroundColor = "#393737";
-      });
+  for (let i = 0; i < size * size; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.className = 'divGrid';
+    container.appendChild(newDiv);
+  }
+
+  // Update the clickDivs variable after creating new divs
+  var clickDivs = document.querySelectorAll('.divGrid');
+
+  // Add a click event to change each div color to black
+  clickDivs.forEach(function (div) {
+    div.addEventListener('click', function () {
+      div.style.backgroundColor = "#393737";
     });
 
+    // Adjust the size of the grid items based on the new size
+    div.style.cssText = `flex: 1 1 calc(450px/${size});`;
+  });
+}
+
+let gridValue = sliderInput.value;
+sliderOutput.textContent = `Grid Size: ${gridValue}`;
+
+sliderInput.addEventListener('input', function () {
+  // Update the variable with the slider value
+  gridValue = sliderInput.value;
+
+  // Update the output text
+  sliderOutput.textContent = `Grid size: ${gridValue}x${gridValue}`;
+
+  // Create or update the grid based on the new size
+  createGrid(gridValue);
+});
+
+// Initial grid creation
+createGrid(gridValue);
